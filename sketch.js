8a276ec8264909aa096aeb1ad58667ca538f7cb8
categorille <categorille@protonmail.com>
2021-02-28 19:48:34 +0100

    food works :)

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ const CANVAS_SIZE_GRID = 20; // canvas size, in grid cell count
 const CANVAS_BACKGROUND_COLOR = 0;
 
 const GRID_CELL_SIZE = CANVAS_SIZE_PX / CANVAS_SIZE_GRID; // aka. grid cell size
-const SNAKE_UPDATE_RATE = FRAME_RATE/2; // snake is updated every `SNAKE_UPDATE_RATE` frames
+const SNAKE_UPDATE_RATE = FRAME_RATE / 4; // snake is updated every `SNAKE_UPDATE_RATE` frames
 const SNAKE_DIRECTION_LEFT = 0;
 const SNAKE_DIRECTION_UP = 1;
 const SNAKE_DIRECTION_RIGHT = 2;
@@ -24,10 +24,11 @@ let snake_dir_next = SNAKE_DIRECTION_RIGHT;
 
 // GLOBAL STATE (food)
 let food_x, food_y;
+let food_has_been_eaten = false;
 
 function setup() {
     // initializations
-    food_x = random(0, CANVAS_SIZE_GRID);
+    generate_new_food();
 
     // settings
     colorMode(HSB, HSB_MAX);
@@ -51,6 +52,7 @@ function draw() {
 
     // TODO optimize: only need to redraw on update, really
     draw_snake();
+    draw_food();
 }
 
 function update_snake() {
@@ -72,11 +74,25 @@ function update_snake() {
             move_snake(0, 1);
             break;
     }
+
+    if (snake_x[0] === food_x
+     && snake_y[0] === food_y) {
+        food_has_been_eaten = true;
+        generate_new_food();
+    }
 }
 
 // advance the entire snake one tile in the direction specified by
 // x and y
 function move_snake(x, y) {
+    // duplicate last body part if the snake has eaten food
+    // (won't cause collisions cause we only check for those with the head)
+    if (food_has_been_eaten) {
+        snake_x.push(snake_x[snake_x.length - 1]);
+        snake_y.push(snake_y[snake_y.length - 1]);
+        food_has_been_eaten = false;
+    }
+
     // move all non-head snake body parts forward, from last to first
     for (let i = snake_x.length - 1; i > 0; --i) {
         snake_x[i] = snake_x[i - 1];
@@ -101,6 +117,18 @@ function move_snake(x, y) {
     }
 }
 
+function generate_new_food() {
+    let conflict = false;
+    do {
+        conflict = false;
+        food_x = Math.floor(random(CANVAS_SIZE_GRID));
+        food_y = Math.floor(random(CANVAS_SIZE_GRID));
+        for (let i = 0; i < snake_x.length; ++i) {
+            conflict = conflict | snake_x[i] === food_x | snake_y[i] === food_y;
+        }
+    } while (conflict);
+}
+
 function draw_snake() {
     for (let i = 0; i < snake_x.length; ++i) {
         const body_part_hue = (snake_hue + i * SNAKE_HUE_OFFSET) % HSB_MAX;
@@ -113,6 +141,16 @@ function draw_snake() {
         );
     }
     snake_hue = (snake_hue + 1) % HSB_MAX;
+}
+
+function draw_food() {
+    fill(color(0, 0, 255));
+    rect(
+        food_x * GRID_CELL_SIZE,
+        food_y * GRID_CELL_SIZE,
+        GRID_CELL_SIZE,
+        GRID_CELL_SIZE
+    );
 }
 
 function keyPressed() {
